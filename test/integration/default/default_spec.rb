9de5ded96aad8a1b7bfe847d94393f81f92d2b40
Muhammad Panji <muhammad.panji@traveloka.com>
2018-05-30 21:51:41 +0700

    fix testing

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,24 +1,13 @@
 # encoding: utf-8
 
-# ntp
-describe package('ntp') do
+# crontab
+describe package('cron') do
   it { should be_installed }
 end
 
-describe service('ntp') do
+describe service('cron') do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
 end
 
-describe ntp_conf do
-  its('driftfile') { should eq '/var/lib/ntp/ntp.drift' }
-  its('server') { should eq [
-      '0.sg.pool.ntp.org',
-      '0.asia.pool.ntp.org',
-      '1.asia.pool.ntp.org',
-      '2.asia.pool.ntp.org',
-      'ntp.ubuntu.com'
-  ]}
-  its('server') { should_not include 'iburst' }
-end
